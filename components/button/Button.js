cc09441765bee201b432dbe2961a0e27118e4647
Andy <github@sockbot.com>
2019-10-04 12:07:07 -0700

    add props to button

--- components/button/Button.js
+++ components/button/Button.js
@@ -1,8 +1,18 @@
 import React from "react";
+import classnames from "classnames";
+
 import "../../src/App.scss";
 
-function Button() {
-  return <div className="button">JUNGLE</div>;
+function Button(props) {
+  const { label, active, onClick } = props;
+  const buttonClass = classnames("button", {
+    active: active
+  });
+  return (
+    <div className={buttonClass} onClick={onClick}>
+      {label}
+    </div>
+  );
 }
 
 export default Button;
--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
+import { storiesOf } from "@storybook/react";
+
 import Button from "../../components/button/Button";
 
 export default {
@@ -7,7 +9,11 @@ export default {
 };
 
 export const button = () => (
-  <Button onClick={action("clicked")}>Hello Button</Button>
+  <Button onClick={action("clicked")} label={"JUNGLE"} active={false} />
 );
 
 // export const text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+
+// storiesOf("Button", module).add("Inactive", () => {
+//   <Button onClick={action("clicked")}>Hello Button</Button>;
+// });
