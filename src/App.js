8be570d61f5b39e73cff9180b2768a05be20f816
Will Wong <willeyw@gmail.com>
2019-10-05 17:00:17 -0700

    added testdata and some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Logo from "./components/Logo";
+import Progressbar from "./components/Progressbar";
+import ButtonGrid from "./components/ButtonGrid";
+import Quickset from "./components/Quickset";
+import { columns, quicksetAction, buttons, quicksets } from "./testdata";
+
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -8,9 +13,26 @@ function App() {
     <main className="layout">
       <section className="samples">Samples</section>
       <section className="middle">
-        <Logo />
+        <section className="logo">
+          <Logo />
+        </section>
+        <section className="progress">
+          <Progressbar />
+        </section>
+        <section className="buttongrid">
+          <ButtonGrid columns={columns} />
+        </section>
       </section>
-      <section className="right">Set Recommended</section>
+      <section className="right">
+        {quicksets.map(quickset => (
+          <Quickset
+            label={quickset.label}
+            color={quickset.color}
+            buttons={quickset.buttons}
+            quicksetAction={quickset.quicksetAction}
+          />
+        ))}
+      </section>
     </main>
   );
 }
