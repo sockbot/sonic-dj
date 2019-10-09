5b128f31f4025280a7d9e77e920f6fab78fef5da
Will Wong <willeyw@gmail.com>
2019-10-08 20:34:07 -0700

    closed second socket

--- src/components/BarCounter.js
+++ src/components/BarCounter.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./BarCounter.scss";
 //import useTrackState from "./useTrackState";
-import openSocket from "socket.io-client";
+import openSocket, socket from "socket.io-client";
 import Progressbar from "./Progressbar";
 //const { trackState, trackSetter } = useTrackState();
+import { socket } from "./websocketfunctions"
 
-const socket = openSocket("http://localhost:8000");
+//const socket = openSocket("http://localhost:8000");
 
 export default class BarCounter extends Component {
   constructor(props) {
